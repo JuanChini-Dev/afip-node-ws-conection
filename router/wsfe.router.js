fc95f1e2ba5d2b25004cfbf5b89788ea58999443
Juan Pablo Chini <chini195@gmail.com>
2024-06-08 20:20:24 -0300

    second Commit

--- router/wsfe.router.js
+++ router/wsfe.router.js
@@ -1,16 +1,15 @@
 import express from "express";
-
-
 import getValidTA from "../services/validateTa.js";
 
 const router = express.Router();
 
-
-
 router.get("/", async (req, res) => {
   try {
     const result = await getValidTA();
-    res.status(200).json(result);
+    if (result) {
+      res.status(200).json(result);
+      
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
